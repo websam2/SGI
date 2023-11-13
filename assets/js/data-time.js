e9f7d0ff5aa6017245ee89677f47509dcc9f3be5
Samuel Frederico Zezilia Cará <scara@cdrg.sp.gov.br>
2023-11-13 17:11:59 -0300

    adicionado, data e hora atual

---
+++ assets/js/data-time.js
@@ -0,0 +1,9 @@
+function showDateTime() {
+  let date = new Date();
+  let options = { timeZone: "America/Sao_Paulo" };
+  let dateString = date.toLocaleDateString("pt-BR", options);
+  let timeString = date.toLocaleTimeString("pt-BR", options);
+  document.getElementById("datetime").innerHTML = dateString + " " + timeString;
+}
+
+setInterval(showDateTime, 1000);
