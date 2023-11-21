e44dd5baec5f0d4959d6211edd1d79ae1a3b0fed
Samuel Frederico Zezilia Cará <scara@cdrg.sp.gov.br>
2023-11-21 13:16:14 -0300

    atualização

--- assets/js/data-time.js
+++ assets/js/data-time.js
@@ -3,7 +3,8 @@ function showDateTime() {
   let options = { timeZone: "America/Sao_Paulo" };
   let dateString = date.toLocaleDateString("pt-BR", options);
   let timeString = date.toLocaleTimeString("pt-BR", options);
-  document.getElementById("datetime").innerHTML = dateString + " " + timeString;
+  document.getElementById("datetime").innerHTML =
+    dateString + " - " + timeString;
 }
 
 setInterval(showDateTime, 1000);
